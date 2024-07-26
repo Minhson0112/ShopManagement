2e44b44f166d726bc0e0cccad3297fdd638b5538
stone <>
2024-07-26 16:58:35 +0900

    sửa lỗi cảnh báo ở controller

--- src/main/java/com/sounshop/ShopManagement/controller/api/productController.java
+++ src/main/java/com/sounshop/ShopManagement/controller/api/productController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
-import java.util.stream.Collectors;
 import java.time.LocalDate;
 
 import com.sounshop.ShopManagement.service.ProductInfoService;
